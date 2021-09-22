3da581595a9a653cce7784e4a007e5f6f6da8163
非显亮之高光铃丰 <MIUUL@outlook.com>
2021-09-22 14:26:48 +0800

    更新。

---
+++ Directory/Directory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptEngine
+{
+    class Directory
+    {
+        public static void MakeDir(string[] Value)
+        {
+            if (!System.IO.Directory.Exists(Value[0])) {
+                System.IO.Directory.CreateDirectory(Value[0]);
+            }
+        }
+    }
+}
